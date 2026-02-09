7e653b33863ab4a24304eea3c8e76300e32d8b22
YuriSoftworksMM <awp.ggexcutor@gmail.com>
2026-02-09 21:12:50 +0900

    Update issue.js

--- api/issue.js
+++ api/issue.js
@@ -1,4 +1,11 @@
 export default async function handler(req, res) {
+  return res.json({
+    length: process.env.KEYAUTH_SELLER_KEY?.length,
+    exists: !!process.env.KEYAUTH_SELLER_KEY
+  });
+}
+
+export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
